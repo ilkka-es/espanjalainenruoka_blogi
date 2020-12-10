87eb70f9cbcfb2e94e32ee08e4ce1acc030eedd8
ilkka-es <8879957+ilkka-es@users.noreply.github.com>
2020-12-10 12:49:30 +0100

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,5 +84,16 @@ module.exports = {
         environments: ['production', 'development']
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-3JC4V8KYR6", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    }
   ],
 }
